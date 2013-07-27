367edd3c3432b3afcd17e467b112a261fc15dcd0
Varinder <Singh>
2013-07-27 14:15:04 +1200

    Lazy loading images
    a dodgy support for lazy loading images

--- picturefill.js
+++ picturefill.js
@@ -1,6 +1,6 @@
 /*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
 
-(function( w ){
+(function( w, $ ){
 
 	// Enable strict mode
 	"use strict";
@@ -24,29 +24,70 @@
 					}
 				}
 
-			// Find any existing img element in the picture element
-			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+				// Find any existing img element in the picture element
+				var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
 
-			if( matches.length ){
-				var matchedEl = matches.pop();
-				if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
-					picImg = w.document.createElement( "img" );
-					picImg.alt = ps[ i ].getAttribute( "data-alt" );
+				if( matches.length ){
+					var matchedEl = matches.pop();
+					if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
+						picImg = w.document.createElement( "img" );
+						picImg.alt = ps[ i ].getAttribute( "data-alt" );
+					}
+
+					if ( isInView(matchedEl) ) {
+						picImg.src =  matchedEl.getAttribute( "data-src" );
+						matchedEl.appendChild( picImg );
+					}
 				}
-
-				picImg.src =  matchedEl.getAttribute( "data-src" );
-				matchedEl.appendChild( picImg );
+				else if( picImg ){
+					picImg.parentNode.removeChild( picImg );
+				}
 			}
-			else if( picImg ){
-				picImg.parentNode.removeChild( picImg );
-			}
-		}
 		}
 	};
+
+	function isInView(elem) {
+		var vpH = getViewportHeight(),
+			scrolltop = ( document.documentElement.scrollTop ?
+				document.documentElement.scrollTop :
+				document.body.scrollTop ),
+			top = getOffsetTop(elem);
+		
+		//console.log("viewport height: ", vpH, " scrolltop: ", scrolltop, " offset top: ", top);
+
+		if ( scrolltop > top || scrolltop + vpH < top ) {
+			return false;
+		} else if ( scrolltop < top ) {
+			return true;
+		}
+	}
+
+	function getOffsetTop(elem) {
+		var top = 0;
+		while (elem.tagName != "BODY") {
+			top = top + elem.offsetTop;
+			elem = elem.offsetParent;
+		}
+		return top;
+	}
+
+	function getViewportHeight() {
+		var height = window.innerHeight; // Safari, Opera
+		var mode = document.compatMode;
+
+		if ( (mode || !$.support.boxModel) ) { // IE, Gecko
+			height = (mode == 'CSS1Compat') ?
+			document.documentElement.clientHeight : // Standards
+			document.body.clientHeight; // Quirks
+		}
+
+		return height;
+	}
 
 	// Run on resize and domready (w.load as a fallback)
 	if( w.addEventListener ){
 		w.addEventListener( "resize", w.picturefill, false );
+		w.addEventListener( "scroll", w.picturefill, false );
 		w.addEventListener( "DOMContentLoaded", function(){
 			w.picturefill();
 			// Run once only
@@ -56,6 +97,7 @@
 	}
 	else if( w.attachEvent ){
 		w.attachEvent( "onload", w.picturefill );
+		w.attachEvent( "onscroll", w.picturefill );
 	}
 
-}( this ));
+}( this, jQuery ));
